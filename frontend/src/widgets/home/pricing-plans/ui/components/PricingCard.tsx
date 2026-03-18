b35cf29abcd318e226ca0f047864056cd651a93c
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-18 21:14:36 +0600

    ui: create PricingCard for PricingPlans comp

---
+++ frontend/src/widgets/home/pricing-plans/ui/components/PricingCard.tsx
@@ -0,0 +1,34 @@
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from './PricingCard.module.scss';
+import Button from '@/shared/ui/Button';
+
+interface PricingCardProps {
+    isPopular?: boolean;
+    heading: string;
+    price: number;
+    features: string[];
+    btnVariant: 'primary' | 'secondary';
+    btnText: string;
+}
+
+const PricingCard = ({ isPopular, heading, price, features, btnVariant, btnText }: PricingCardProps) => {
+    return (
+        <div className={`${styles['pricing-card']} ${isPopular ? styles.popular : ''}`}>
+            {isPopular && <div className={styles['popular-badge']}>Самый популярный</div>}
+            <h3>{heading}</h3>
+            <div className={styles.price}>{price} ₽ <span>/мес</span></div>
+            <ul>
+                {features.map((feature, index) => (
+                    <li key={index}>
+                        <FontAwesomeIcon icon={faCheck} /> {feature}
+                    </li>
+                ))}
+            </ul>
+            <Button variant={btnVariant}>{btnText}</Button>
+        </div>
+    );
+};
+
+export default PricingCard;
