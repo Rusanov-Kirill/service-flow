e41853b9cdde739f5bf7a3dee4e29e9edb7f2793
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-18 16:43:37 +0600

    ui: create TargetAudience widget

---
+++ frontend/src/widgets/target-audience/ui/TargetAudience.tsx
@@ -0,0 +1,52 @@
+import { faStore, faUserClock } from '@fortawesome/free-solid-svg-icons';
+
+import SectionHeading from '@/shared/ui/SectionHeading';
+
+import styles from './TargetAudience.module.scss';
+import AudienceCard from './components/AudienceCard';
+
+const TargetAudience = () => {
+    const specialistFeatures = [
+        'Календарь и онлайн-запись 24/7',
+        'Финансовый учёт и отчёты',
+        'Клиентская база и история',
+        'Напоминания клиентам'
+    ];
+
+    const clientFeatures = [
+        'Поиск по категориям и городу',
+        'Реальное свободное время',
+        'Напоминания о записи',
+        'История визитов'
+    ];
+
+    return (
+        <section className={styles['for-whom']}>
+            <div className={styles.wrapper}>
+                <SectionHeading heading='Для кого ServiceFlow'>
+                    Универсальное решение для двух миров
+                </SectionHeading>
+                <div className={styles['audience-grid']}>
+                    <AudienceCard 
+                        heading='Специалистам и бизнесу'
+                        description='Всё для управления услугами: от самозанятого мастера до небольшой студии с сотрудниками'
+                        audienceIcon={faStore}
+                        features={specialistFeatures}
+                        btnVariant='primary'
+                        btnText='Начать работать'
+                    />
+                    <AudienceCard 
+                        heading='Клиентам и покупателям'
+                        description='Удобный способ находить специалистов и записываться без звонков и очередей'
+                        audienceIcon={faUserClock}
+                        features={clientFeatures}
+                        btnVariant='secondary'
+                        btnText='Найти мастера'
+                    />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default TargetAudience;
