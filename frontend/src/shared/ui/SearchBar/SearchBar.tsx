798aa9cdd411a62f8e66d7abf4aef61ca4894387
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-18 13:38:39 +0600

    ui: create SearchBar component

---
+++ frontend/src/shared/ui/SearchBar/SearchBar.tsx
@@ -0,0 +1,10 @@
+import type { InputHTMLAttributes } from 'react';
+import styles from './SearchBar.module.scss';
+
+const SearchBar = ({ className, ...props }: InputHTMLAttributes<HTMLInputElement>) => {
+    return (
+        <input className={className || styles.input}  {...props}></input>
+    );
+};
+
+export default SearchBar;
