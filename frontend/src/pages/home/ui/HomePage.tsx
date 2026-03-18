6fe52fbab4ea33408d913e3b6b1a647c9ce56758
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-18 13:35:34 +0600

    ui: create HomePage component

---
+++ frontend/src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from "@/widgets/header";
+import DualHero from "@/widgets/dual-hero";
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <DualHero />
+        </>
+    );
+};
+
+export default HomePage;
