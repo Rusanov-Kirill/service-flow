a47e1614fdd573f8e8e4e9e4afb5d39f6f3c6ef6
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-18 13:45:34 +0600

    ui: create DualHero component

---
+++ frontend/src/widgets/dual-hero/ui/DualHero.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import styles from './DualHero.module.scss';
+import HeroSwitcher from './HeroSwitcher.tsx';
+import SpecialistPanel from './panels/SpecialistPanel.tsx';
+import ClientPanel from './panels/ClientPanel.tsx';
+
+const DualHero = () => {
+    const [isSpecialist, setIsSpecialist] = useState<boolean>(true);
+
+    return (
+        <section className={styles['dual-hero']}>
+            <div className={styles.wrapper}>
+                <HeroSwitcher
+                    isSpecialist={isSpecialist}
+                    onSwitch={setIsSpecialist}
+                />
+                <div className={styles['hero-container']}>
+                    <div key={isSpecialist ? 'spec' : 'client'} className={styles['hero-animation']}>
+                        {isSpecialist ? <SpecialistPanel /> : <ClientPanel />}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default DualHero;
