013cadb5215ec3a1eaa8cc752a94761843684365
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-19 21:21:30 +0600

    ui: complete HomePage component

--- frontend/src/pages/home/ui/HomePage.tsx
+++ frontend/src/pages/home/ui/HomePage.tsx
@@ -1,11 +1,21 @@
-import DualHero from "@/widgets/dual-hero";
-import Header from "@/widgets/header";
+import DualHero from "@/widgets/home/dual-hero";
+import Footer from "@/widgets/home/footer";
+import Header from "@/widgets/home/header";
+import PlatformValues from "@/widgets/home/platform-values";
+import PricingPlans from "@/widgets/home/pricing-plans";
+import SpecialistCapabilities from "@/widgets/home/specialist-capabilities";
+import TargetAudience from "@/widgets/home/target-audience";
 
 const HomePage = () => {
     return (
         <>
             <Header />
             <DualHero />
+            <PlatformValues />
+            <TargetAudience />
+            <SpecialistCapabilities />
+            <PricingPlans />\
+            <Footer />
         </>
     );
 };
