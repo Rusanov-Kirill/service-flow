7465b69559439987c137c621063c1db64d58c464
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-23 00:09:42 +0600

    styles: add responsive design for header comp

--- frontend/src/widgets/home/header/ui/Header.tsx
+++ frontend/src/widgets/home/header/ui/Header.tsx
@@ -1,29 +1,57 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { HomePageSectionRefsContext } from '@/pages/home/context';
 import Button from '@shared/ui/Button';
+import { CloseIcon } from '@/shared/ui/icons/CloseIcon';
+import { MenuIcon } from '@/shared/ui/icons/MenuIcon';
 import Logo from '@shared/ui/Logo';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
     const refs = useContext(HomePageSectionRefsContext);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const handleScrollValues = () => refs?.platformValuesRef?.current?.scrollIntoView();
-    const handleScrollAudience = () => refs?.targetAudienceRef?.current?.scrollIntoView();
-    const handleScrollCapabilities = () => refs?.specialistCapabilitiesRef?.current?.scrollIntoView();
-    const handleScrollPricing = () => refs?.pricingRef?.current?.scrollIntoView();
+    const handleScrollValues = () => {
+        refs?.platformValuesRef?.current?.scrollIntoView();
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    const handleScrollAudience = () => {
+        refs?.targetAudienceRef?.current?.scrollIntoView();
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    const handleScrollCapabilities = () => {
+        refs?.specialistCapabilitiesRef?.current?.scrollIntoView();
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    const handleScrollPricing = () => {
+        refs?.pricingRef?.current?.scrollIntoView();
+        setIsMenuOpen(!isMenuOpen);
+    }
 
     return (
         <header className={styles.header}>
             <div className={styles.wrapper}>
                 <Logo type='header' />
-                <nav className={styles['nav-links']}>
-                    <button onClick={handleScrollValues}>Ценности</button>
-                    <button onClick={handleScrollAudience}>Аудитория</button>
-                    <button onClick={handleScrollCapabilities}>Для специалистов</button>
-                    <button onClick={handleScrollPricing}>Тарифы</button>
-                </nav>
+                <Button
+                    className={styles.burger}
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    aria-label='Меню'
+                >
+                    {isMenuOpen ? <MenuIcon /> : <CloseIcon />}
+                </Button>
+                <div className={`${styles['dropdown-menu']} ${isMenuOpen ? styles.open : ''}`}>
+                    <nav className={styles['nav-links']}>
+                        <button onClick={handleScrollValues}>Ценности</button>
+                        <button onClick={handleScrollAudience}>Аудитория</button>
+                        <button onClick={handleScrollCapabilities}>Для специалистов</button>
+                        <button onClick={handleScrollPricing}>Тарифы</button>
+                    </nav>
+                    <Button className={styles['sign-in']}>Войти</Button>
+                </div>
                 <div className={styles['auth-buttons']}>
                     <Button variant='secondary'>Войти</Button>
                     <Button variant='primary'>Регистрация</Button>
