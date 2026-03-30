d6e05907100b50110767c939dbd92b4444877b47
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-30 19:52:43 +0600

    feat: create custom redirect hook

---
+++ frontend/src/shared/utils/useRedirect.ts
@@ -0,0 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@/app/router/config';
+
+export const useRedirect = () => {
+    const navigate = useNavigate();
+
+    return {
+        redirectToLogin: () => navigate(ROUTES.AUTH.LOGIN),
+        redirectToRegister: () => navigate(ROUTES.AUTH.REGISTER),
+        redirectToHome: () => navigate(ROUTES.HOME),
+    };
+};
