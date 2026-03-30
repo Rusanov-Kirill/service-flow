f6889775432a26067c11ea326903eeb629996a0f
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-03-30 19:41:56 +0600

    feat: add RouterProvider to App comp

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,11 @@
-import '@app/styles';
-import HomePage from '@/pages/home';
+import { RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <HomePage />
-  );
-}
+import '@app/styles';
+
+import { router } from './router';
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
