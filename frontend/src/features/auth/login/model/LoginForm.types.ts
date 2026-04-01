28e570cfdcda66c9f865163f4523e07dea324f45
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-01 21:48:26 +0600

    model: delete login password validation

--- frontend/src/features/auth/login/model/LoginForm.types.ts
+++ frontend/src/features/auth/login/model/LoginForm.types.ts
@@ -2,13 +2,7 @@ import { z } from 'zod';
 
 export const loginSchema = z.object({
     email: z.string().email('Неверный формат email'),
-    password: z
-        .string()
-        .min(8, 'Пароль должен состоять минимум из 8 символов')
-        .regex(
-            /^[a-zA-Z0-9!@#$%^&*()_+\-=[]{}]*$/,
-            'Пароль может содержать только английские буквы, цифры и символы !@#$%^&*()_+-=[]{}'
-        )
+    password: z.string().min(1, 'Введите пароль')
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
