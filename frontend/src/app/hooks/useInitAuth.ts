6b9bf392166e205fd8c87bd5120159e515ca1e69
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-02 23:12:07 +0600

    feat: create hook to restore session after page update

---
+++ frontend/src/app/hooks/useInitAuth.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useAuthStore } from '@app/store/authStore';
+import { authApi } from '@/shared/api/authApi';
+
+export const useInitAuth = () => {
+    const setAuth = useAuthStore((state) => state.setAuth);
+    const logout = useAuthStore((state) => state.logout);
+    const setInitialized = useAuthStore((s) => s.setInitialized);
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const response = await authApi.refresh();
+
+                if (response.data.success) {
+                    setAuth(
+                        response.data.data.accessToken,
+                        response.data.data.user
+                    );
+                }
+            } catch {
+                logout();
+            } finally {
+                setInitialized();
+            }
+        };
+
+        init();
+    }, []);
+};
