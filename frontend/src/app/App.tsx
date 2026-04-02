dcdc707b4d84e5ebd00ce558c7bcb6055e518820
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-02 23:13:14 +0600

    refactor: add useInitAuth hook at App

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,9 +2,12 @@ import { RouterProvider } from 'react-router-dom';
 
 import '@app/styles';
 
+import { useInitAuth } from './hooks/useInitAuth';
 import { router } from './router';
 
 const App = () => {
+  useInitAuth();
+
   return <RouterProvider router={router} />;
 };
 
