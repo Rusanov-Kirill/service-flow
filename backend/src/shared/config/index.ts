bdb68a58830374bfe4d53d593dff0bf19bc6b129
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-02 13:51:17 +0600

    config: modify env variables config

--- backend/src/shared/config/index.ts
+++ backend/src/shared/config/index.ts
@@ -4,10 +4,9 @@ dotenv.config();
 
 const requiredEnvVars = [
     'DATABASE_URL',
-    'RESEND_API_KEY',
-    'FRONTEND_URL',
     'JWT_SECRET',           
-    'JWT_REFRESH_SECRET'    
+    'JWT_REFRESH_SECRET',
+    'RESEND_API_KEY'    
 ];
 
 for (const envVar of requiredEnvVars) {
@@ -32,8 +31,10 @@ export const config = {
     },
     
     resend: {
-        apiKey: process.env['RESEND_API_KEY'] as string
+        apiKey: process.env['RESEND_API_KEY'] as string,
+        fromEmail: process.env['RESEND_FROM_EMAIL'] || 'onboarding@resend.dev'
     },
 
-    frontendUrl: process.env['FRONTEND_URL'] as string
+    frontendUrl: process.env['FRONTEND_URL'] || 'http://localhost:5173',
+    backendUrl: process.env['BACKEND_URL'] || 'http://localhost:5000'
 };
