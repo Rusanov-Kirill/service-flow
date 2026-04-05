62229258012aa8aa71ecc776db9d6d4eb47b77ab
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-05 19:45:50 +0600

    routes: update redirect page after success login

--- frontend/src/app/router/GuestRoute.tsx
+++ frontend/src/app/router/GuestRoute.tsx
@@ -15,7 +15,7 @@ const GuestRoute = ({ children }: Props) => {
     }
 
     if (accessToken) {
-        return <Navigate to={ROUTES.HOME} replace />;
+        return <Navigate to={ROUTES.DASHBOARD} replace />;
     }
 
     return children;
