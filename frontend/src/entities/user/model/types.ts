d0ca8538676297cd86c46dea1a897089a7ce3c07
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-09 21:54:31 +0600

    model: update User entity on frontend

--- frontend/src/entities/user/model/types.ts
+++ frontend/src/entities/user/model/types.ts
@@ -1,3 +1,5 @@
+import type { Company } from "@/entities/company";
+
 export interface User {
     id: string;
     email: string;
@@ -7,6 +9,7 @@ export interface User {
     phoneNumber?: string;
     emailVerified: boolean;
     role?: 'user' | 'admin' | 'owner';
+    companies: Company[]
     isActive?: boolean;
     lastLogin?: string;
     createdAt?: string;
