d614f9312c42fb5fc503eb4866e1d955f7d3e137
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-09 19:46:32 +0600

    feat: create responsive PlaceholderLogo comp

---
+++ frontend/src/shared/ui/PlaceholderLogo/PlaceholderLogo.tsx
@@ -0,0 +1,28 @@
+import companyLogo from "@/shared/assets/images/company-image-placeholder.svg";
+import profileLogo from "@/shared/assets/images/user-placholder.jpg";
+
+interface PlaceholderLogoProps {
+    src?: string
+    alt?: string
+    className?: string
+    variant: 'company' | 'profile'
+}
+
+const PlaceholderLogo = ({ src, alt, className, variant = 'company' }: PlaceholderLogoProps) => {
+    const placeholder = variant === 'company' ? companyLogo : profileLogo;
+    return (
+        <img
+            src={src || placeholder}
+            alt={alt ? `Логотип ${alt}` : 'Логотип'}
+            className={className}
+            loading="lazy"
+            onError={(e) => {
+                const target = e.currentTarget;
+                target.onerror = null;
+                target.src = placeholder;
+            }}
+        />
+    )
+}
+
+export default PlaceholderLogo;
