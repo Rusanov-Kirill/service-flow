5b8b82789d8e2b08eb3831feecc0e42e1f024999
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-09 21:53:05 +0600

    feat: recieve data from auth store instead of mocks

--- frontend/src/widgets/dashboard/profile/ui/Profile.tsx
+++ frontend/src/widgets/dashboard/profile/ui/Profile.tsx
@@ -1,35 +1,24 @@
 import { useState } from 'react';
 
+import { useAuthStore } from '@/app/store/useAuthStore';
 import EditProfileModal from '@/features/edit-profile';
 import PlaceholderLogo from '@/shared/ui/PlaceholderLogo';
 
 import styles from './Profile.module.scss';
 
-const mockUser = {
-  email: 'user@mail.com',
-  firstName: 'Иван',
-  lastName: 'Иванов',
-  emailVerified: false,
-  avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsWnOH5VcpJMtinN689lmKB0ebMS_ttbvS3Q&s',
-  phoneNumber: '+7 (547) 123-45-67',
-  lastLogin: '2026-04-08',
-  createdAt: '2025-12-01',
-  companies: [
-    { name: 'Barbershop', slug: 'barbershop' },
-    { name: 'Yoga Studio', slug: 'yoga-studio' },
-  ],
-};
-
 const Profile = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { user } = useAuthStore();
+
+  if (!user) return null;
 
   return (
     <div className={styles.profile}>
       <div className={styles.header}>
         <div className={styles.avatar}>
           <PlaceholderLogo
-            src={mockUser.avatar}
-            alt={`Аватар пользователя ${mockUser.firstName + '' + mockUser.lastName}`}
+            src={user.avatar}
+            alt={`Аватар пользователя ${user.firstName + '' + user.lastName}`}
             className={styles.avatar}
             variant='profile'
           />
@@ -37,7 +26,7 @@ const Profile = () => {
 
         <div className={styles.info}>
           <h2>
-            {mockUser.firstName} {mockUser.lastName}
+            {user.firstName} {user.lastName}
           </h2>
         </div>
 
@@ -48,7 +37,7 @@ const Profile = () => {
 
       {isOpen && (
         <EditProfileModal
-          user={mockUser}
+          user={user}
           onClose={() => setIsOpen(false)}
         />
       )}
@@ -60,9 +49,9 @@ const Profile = () => {
           <div className={styles.item}>
             <span className={styles.label}>Электронная почта</span>
             <p className={styles.emailRow}>
-              {mockUser.email}
+              {user.email}
 
-              {!mockUser.emailVerified && (
+              {!user.emailVerified && (
                 <span className={styles.unverified}>
                   Не подтверждена
                 </span>
@@ -72,7 +61,7 @@ const Profile = () => {
 
           <div className={styles.item}>
             <span className={styles.label}>Телефон</span>
-            <p>{mockUser.phoneNumber}</p>
+            <p>{user.phoneNumber || 'Не добавлен'}</p>
           </div>
         </div>
       </div>
@@ -83,12 +72,12 @@ const Profile = () => {
         <div className={styles.grid}>
           <div className={styles.item}>
             <span className={styles.label}>Последний вход</span>
-            <p>{mockUser.lastLogin}</p>
+            <p>{user.lastLogin ? new Date(user.lastLogin).toLocaleDateString('ru-RU') : '—'}</p>
           </div>
 
           <div className={styles.item}>
             <span className={styles.label}>Дата регистрации</span>
-            <p>{mockUser.createdAt}</p>
+            <p>{user.createdAt ? new Date(user.createdAt).toLocaleDateString('ru-RU') : '—'}</p>
           </div>
         </div>
       </div>
@@ -97,13 +86,13 @@ const Profile = () => {
         <h4>Мои компании</h4>
 
         <div className={styles.companies}>
-          {mockUser.companies.length === 0 ? (
+          {!(user.companies && user.companies.length > 0) ? (
             <div className={styles.empty}>
               У вас еще нету действующих компаний
             </div>
           ) : (
             <div className={styles.companies}>
-              {mockUser.companies.map((c) => (
+              {user.companies?.map((c) => (
                 <div key={c.slug} className={styles.companyCard}>
                   <div>
                     <p className={styles.companyName}>{c.name}</p>
