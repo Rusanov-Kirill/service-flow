468ecf502cf7b80f46e73b2fcd5b761ae1244111
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-10 19:51:10 +0600

    feat: create user module types and validation files

---
+++ backend/src/modules/user/user.types.ts
@@ -0,0 +1,20 @@
+export interface UpdateProfileInput {
+    firstName?: string;
+    lastName?: string;
+    phoneNumber?: string;
+    avatar?: string;
+}
+
+export interface UserResponse {
+    id: string;
+    email: string;
+    firstName: string | null;
+    lastName: string | null;
+    avatar: string | null;
+    phoneNumber: string | null;
+    emailVerified: boolean;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+    lastLogin: Date | null,
+}
