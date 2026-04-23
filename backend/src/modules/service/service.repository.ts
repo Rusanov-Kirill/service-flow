242447edd1b4bc3b068fa0bcd86701e8cfdd752b
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-23 20:47:57 +0600

    feat: create findById method at service.repository file

--- backend/src/modules/service/service.repository.ts
+++ backend/src/modules/service/service.repository.ts
@@ -4,9 +4,15 @@ import { CreateServiceDto } from './service.types';
 export const serviceRepository = {
     createMany: async (services: CreateServiceDto[]): Promise<void> => {
         if (services.length === 0) return;
-        
+
         await prisma.service.createMany({
             data: services,
+        });
+    },
+
+    findById: async (id: string) => {
+        return prisma.service.findUnique({
+            where: { id },
         });
     },
 }
