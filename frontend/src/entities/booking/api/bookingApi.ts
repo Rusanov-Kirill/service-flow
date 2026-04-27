565de6d30502acb3fdcf824a206cfd5fc99e5642
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-04-27 18:27:22 +0600

    api: create booking api methods on frontend

---
+++ frontend/src/entities/booking/api/bookingApi.ts
@@ -0,0 +1,25 @@
+import { apiClient } from "@/shared/api/client";
+
+import type { Booking, CreateCustomerDto, UpdateBookingDto } from "../model/types";
+
+type BookingCreateDto = Booking & CreateCustomerDto;
+
+export const bookingApi = {
+    create: async (data: BookingCreateDto) => {
+        const response = await apiClient.post('/bookings', data);
+        return response.data;
+    },
+
+    update: async (id: string, data: UpdateBookingDto) => {
+        const response = await apiClient.patch(`/bookings/${id}`, data)
+        return response.data;
+    },
+
+    getBookedSlots: async (companyId: string, date: string, serviceId?: string): Promise<string[]> => {
+        const params = new URLSearchParams({ date });
+        if (serviceId) params.append('serviceId', serviceId);
+
+        const response = await apiClient.get(`/bookings/${companyId}/booked-slots?${params}`);
+        return response.data;
+    },
+};
