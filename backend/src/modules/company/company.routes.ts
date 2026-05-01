f67bf5d654d1b61b390fa6205b9f9b43703af342
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-05-01 21:25:13 +0600

    feat: add new endpoints to company routes related to members

--- backend/src/modules/company/company.routes.ts
+++ backend/src/modules/company/company.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { companyController } from './company.controller';
+import { companyMemberController } from '../company_member/company_member.controller';
 
 const router = Router();
 
 router.get('/slug/:slug', companyController.getBySlug);
 router.get('/owner/:ownerId', companyController.getByOwnerId);
+
+router.get('/:companyId/members', companyMemberController.getAllByCompanyId);
+router.get('/:companyId/members/email/:email', companyMemberController.getByEmail);
+router.get('/:companyId/members/user/:userId', companyMemberController.getByUserId);
+
 router.get('/:id', companyController.getById);
 router.get('/', companyController.getAll);
 router.post('/', companyController.createWithServices);
