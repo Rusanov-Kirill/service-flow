b56845cb80a67fe60beb9c566bf10c4dc116f996
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-05-04 21:36:25 +0600

    feat: add getByUserId method to company member module routes ob backend

--- backend/src/modules/company_member/company_member.routes.ts
+++ backend/src/modules/company_member/company_member.routes.ts
@@ -3,9 +3,11 @@ import { companyMemberController } from './company_member.controller';
 
 const router = Router();
 
+router.get('/user/:userId/company/:companyId', companyMemberController.getByUserId);
 router.get('/:id', companyMemberController.getById);
+
 router.post('/', companyMemberController.create);
-router.put('/:id', companyMemberController.update);
+router.patch('/:id', companyMemberController.update);
 router.delete('/:id', companyMemberController.delete);
 
 export default router;
