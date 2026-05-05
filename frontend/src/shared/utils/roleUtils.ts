4e2c4267dd3b69218178a72583c41d01a5fd6c15
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-05-05 20:30:12 +0600

    feat: create utils that provide users roles on frontend

---
+++ frontend/src/shared/utils/roleUtils.ts
@@ -0,0 +1,7 @@
+export const roleLabels: Record<string, string> = {
+  owner: 'Владелец',
+  admin: 'Системный администратор',
+  manager: 'Управляющий',
+  receptionist: 'Администратор',
+  member: 'Сотрудник'
+};
